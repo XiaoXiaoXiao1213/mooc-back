1a2286edf7c91e13a84a31104d2a10f55e201935
conanxiao <conanxiao@tencent.com>
2021-03-27 14:11:55 +0800

    order

--- apis/web/order.go
+++ apis/web/order.go
@@ -3,7 +3,6 @@ package web
 import (
 	"github.com/kataras/iris"
 	"github.com/sirupsen/logrus"
-	"management/core"
 	"management/core/orders"
 	"management/core/users"
 	"management/infra"
@@ -23,22 +22,19 @@ func (a *OrderApi) Init() {
 	a.service = orders.GetOrderService()
 	groupRouter := base.Iris().Party("/api/1.0/order")
 	groupRouter.Post("/create", loginMeddle, employeeMeddle, a.create)
-	groupRouter.Get("/{id}", loginMeddle, a.orderId)
+	groupRouter.Get("/{id}", loginMeddle, a.getOrderById)
 	groupRouter.Post("/{id}", loginMeddle, a.editOrder)
 	groupRouter.Post("/evaluation/{order_id}", loginMeddle, a.evaluationOrder)
 }
 
 // 创建订单
 func (a *OrderApi) create(ctx iris.Context) {
-	//获取请求参数
-	phone := ctx.GetHeader("phone")
-	user := users.User{Phone: phone}
-	order := orders.Order{}
-	err := ctx.ReadJSON(&order)
 	r := base.Res{
 		Code: base.ResCodeOk,
 	}
-	// TODO 参数检测
+
+	order := orders.Order{}
+	err := ctx.ReadJSON(&order)
 	if err != nil {
 		r.Code = base.ResError
 		r.Message = "字段或字段值格式错误"
@@ -47,26 +43,26 @@ func (a *OrderApi) create(ctx iris.Context) {
 		return
 	}
 
-	//创建用户
+	//获取请求参数
+	phone := ctx.GetHeader("phone")
+	user := users.User{Phone: phone}
 	err = a.service.Create(order, user)
 	if err != nil {
 		r.Code = base.ResError
 		r.Message = err.Error()
 		logrus.Error(err)
-		ctx.JSON(&r)
-		return
 	}
-
 	ctx.JSON(&r)
-
 }
+
 func (a *OrderApi) evaluationOrder(ctx iris.Context) {
-	//获取请求参数
-	evalutaion := orders.Evaluation{}
-	err := ctx.ReadJSON(&evalutaion)
 	r := base.Res{
 		Code: base.ResCodeOk,
 	}
+
+	//获取请求参数
+	evalutaion := orders.Evaluation{}
+	err := ctx.ReadJSON(&evalutaion)
 	if err != nil {
 		r.Code = base.ResError
 		r.Message = "字段或字段值格式错误"
@@ -75,21 +71,16 @@ func (a *OrderApi) evaluationOrder(ctx iris.Context) {
 		return
 	}
 
-	//创建用户
 	err = a.service.TakeEvaluation(evalutaion)
 	if err != nil {
 		r.Code = base.ResError
 		r.Message = err.Error()
 		logrus.Error(err)
-		ctx.JSON(&r)
-		return
 	}
-
 	ctx.JSON(&r)
-
 }
 
-func (a *OrderApi) orderId(ctx iris.Context) {
+func (a *OrderApi) getOrderById(ctx iris.Context) {
 	r := base.Res{
 		Code: base.ResCodeOk,
 	}
@@ -103,6 +94,7 @@ func (a *OrderApi) orderId(ctx iris.Context) {
 		logrus.Error(err)
 		return
 	}
+
 	order, err := a.service.GetOrdersById(id)
 	if err != nil {
 		r.Code = base.ResError
@@ -111,24 +103,13 @@ func (a *OrderApi) orderId(ctx iris.Context) {
 		logrus.Error(err)
 		return
 	}
-	phone := ctx.GetHeader("phone")
-	userId, _ := strconv.ParseInt(ctx.GetHeader("user_id"), 10, 64)
-	userType, _ := strconv.Atoi(ctx.GetHeader("user_type"))
-	user := users.User{
-		Phone:    phone,
-		Id:       userId,
-		UserType: userType,
-	}
-	token, _ := core.GenerateToken(user)
 	r.Data = map[string]interface{}{
 		"order": order,
-		"token": token,
+		"token": refreshToken(ctx),
 	}
 	ctx.JSON(&r)
-
 }
 
-// 创建订单
 func (a *OrderApi) editOrder(ctx iris.Context) {
 	r := base.Res{
 		Code: base.ResCodeOk,
@@ -143,14 +124,7 @@ func (a *OrderApi) editOrder(ctx iris.Context) {
 		logrus.Error(err)
 		return
 	}
-	order, err := a.service.GetOrdersById(id)
-	if err != nil {
-		r.Code = base.ResError
-		r.Message = "找不到订单"
-		ctx.JSON(&r)
-		logrus.Error(err)
-		return
-	}
+
 	stage := orders.OrderStage{}
 	err = ctx.ReadJSON(&stage)
 	if err != nil {
@@ -160,18 +134,16 @@ func (a *OrderApi) editOrder(ctx iris.Context) {
 		logrus.Error(err)
 		return
 	}
-	phone := ctx.GetHeader("phone")
-	userId, _ := strconv.ParseInt(ctx.GetHeader("user_id"), 10, 64)
-	userType, _ := strconv.Atoi(ctx.GetHeader("user_type"))
-	user := users.User{
-		Phone:    phone,
-		Id:       userId,
-		UserType: userType,
+	err = a.service.EditStage(stage)
+	if err != nil {
+		r.Code = base.ResError
+		r.Message = err.Error()
+		ctx.JSON(&r)
+		logrus.Error(err)
+		return
 	}
-	token, _ := core.GenerateToken(user)
 	r.Data = map[string]interface{}{
-		"order": order,
-		"token": token,
+		"token": refreshToken(ctx),
 	}
 	ctx.JSON(&r)
 
