fd017006720ba120cab7c14fb85ae88d75ac3225
conanxiao <conanxiao@tencent.com>
2021-03-27 14:13:21 +0800

    delete

--- app.go
+++ app.go
@@ -10,7 +10,6 @@ import (
 func init() {
 	infra.Register(&base.PropsStarter{})
 	infra.Register(&base.DbxDatabaseStarter{})
-	infra.Register(&base.ValidatorStarter{})
 	infra.Register(&base.IrisServerStarter{})
 	infra.Register(&infra.WebApiStarter{})
 	infra.Register(&base.HookStarter{})
